755aa679f2e56df532263c120b7e171c588663e1
Yasimo <yanmoura142529@gmail.com>
2020-03-25 02:31:30 -0300

    Paginação e adição do CORS

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -13,23 +13,3 @@ module.exports = {
         return response.json(ong);
     }
 }
-
-// const connection = require('../database/connection');
-
-// module.exports = {
-//   async create(req, res) {
-//     const { id } = req.body;
-
-//     const ong = await connection('ongs')
-//       .where('id', id)
-//       .select('name')
-//       .first();
-
-//     if (!ong) {
-//       return res.status(400).json({ error: 'ONG não encontrada' })
-//     };
-
-//     return res.status(200).json(ong);
-//   }
-
-// }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -19,7 +20,7 @@ const app = express();
  * Route Params: Parâmetros utilizados para identificar recursos
  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
  */
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
